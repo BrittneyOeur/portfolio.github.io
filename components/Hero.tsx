92eb5784ea87023d68434321d24e6329fb48774d
Brittney Oeur <oeur.brittney@gmail.com>
2025-10-26 13:22:04 -0700

    Adjustment

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
     <div className="relative min-h-screen">
         <div className="pb-20 pt-20 lg:pt-30 lg:pb-30">
             <div className="flex flex-col lg:flex-row justify-center items-center relative my-12 z-10 gap-24">
-                <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col">
+                <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col p-0">
                     <div className="mb-14">
                         <TextGenerateEffect
                             className="text-center text-[32px] md:text-4xl lg:text-4xl font-bold"
@@ -33,7 +33,7 @@ const Hero = () => {
                     />
                     <div className="mt-16">
                         <FadingText
-                            className="text-center md:tracking-wider mb-20 text-sm md:text-md lg:text-[18px]"
+                            className="text-center justify-center items-center md:tracking-wider text-sm md:text-md lg:text-[18px]"
                             delay={0.2}
                         />
                     </div>
